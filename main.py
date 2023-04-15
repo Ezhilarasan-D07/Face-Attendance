02c96eff6468a18254406ecd47f517e6da2dbaf4
Ezhilarasan <130550282+Ezhilarasan-D07@users.noreply.github.com>
2023-04-15 02:30:14 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
-def counter(array):
-    flag=0
-    for i in array:
-        flag+=i
-    return flag 
+from systools import reduce
+def counter(a,b):  
+    return a+b
+#for i in array:
+#flag+=i
+#return flag 
+
 array=[2,1,3,4,5]
-print(counter(array))
+
+#(counter(array))
 #2+1+3+4+5=--> 15
 
+result = reduce(counter(a,b),array)
+print(result)
